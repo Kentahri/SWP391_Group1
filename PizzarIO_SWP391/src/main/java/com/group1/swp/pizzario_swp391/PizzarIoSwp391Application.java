7daa2797d0734ef1c79403244a83d715a03f71d6
Haichan <160374468+DaeyangTQH@users.noreply.github.com>
2025-09-21 23:17:00 +0700

    Initial project

---
+++ PizzarIO_SWP391/src/main/java/com/group1/swp/pizzario_swp391/PizzarIoSwp391Application.java
@@ -0,0 +1,13 @@
+package com.group1.swp.pizzario_swp391;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzarIoSwp391Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzarIoSwp391Application.class, args);
+    }
+
+}
---
+++ PizzarIO_SWP391/src/test/java/com/group1/swp/pizzario_swp391/PizzarIoSwp391ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group1.swp.pizzario_swp391;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzarIoSwp391ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
