c5c179bce3b15258de3a17fd663c2af0670eb5c0
VinhNN747 <vinhngoc172005@gmail.com>
2025-09-24 16:09:25 +0700

    add(dto cho category): tạo dto cho category và sử dụng annotation lombok. refactor(category): bỏ các getter và setter và constructor có thể thay thế đc bằng annotations của lombok.

--- PizzarIO_SWP391/src/main/java/com/group1/swp/pizzario_swp391/dto/CategoryDTO.java
+++ PizzarIO_SWP391/src/main/java/com/group1/swp/pizzario_swp391/dto/CategoryDTO.java
@@ -1,4 +1,17 @@
 package com.group1.swp.pizzario_swp391.dto;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class CategoryDTO {
+    Long id;
+    String name;
+    String description;
+    boolean isActive;
 }
+
--- PizzarIO_SWP391/src/main/java/com/group1/swp/pizzario_swp391/entity/Category.java
+++ PizzarIO_SWP391/src/main/java/com/group1/swp/pizzario_swp391/entity/Category.java
@@ -1,11 +1,8 @@
 package com.group1.swp.pizzario_swp391.entity;
 
 import jakarta.persistence.*;
-import jakarta.persistence.Table;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -14,16 +11,31 @@ import java.util.List;
 @Entity
 @Table(name = "[Category]")
 @Data
-@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Category {
 
-    @OneToMany(mappedBy = "category")
-    private List<Product> products;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
 
-    public Category() {
-        products = new ArrayList<>();
-    }
+    String name;
+    String description;
 
+    @Column(name = "is_active")
+    boolean isActive;
+
+    @Column(name = "created_at")
+    LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    List<Product> products = new ArrayList<>();
+
+    // helper methods
     public void addProduct(Product product) {
         products.add(product);
         product.setCategory(this);
@@ -32,62 +44,5 @@ public class Category {
     public void removeProduct(Product product) {
         products.remove(product);
         product.setCategory(null);
-    }
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String name;
-    private String description;
-
-    @Column(name = "is_active")
-    private boolean isActive;
-
-    @Column(name = "created_at")
-    private LocalDateTime createdAt;
-
-    @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
-
-    public Category(String name, String description, boolean isActive, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        this();
-        this.name = name;
-        this.description = description;
-        this.isActive = isActive;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setCreatedAt(LocalDateTime now) {
-        this.createdAt = now;
-    }
-
-    public void setUpdatedAt(LocalDateTime now) {
-        this.updatedAt = now;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
-    public void setActive(boolean active) {
-        isActive = active;
     }
 }
