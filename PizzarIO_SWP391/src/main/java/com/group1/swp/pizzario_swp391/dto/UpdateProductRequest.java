176d13a026464a8b9c2eb40151b151972c01b2cd
VinhNN747 <vinhngoc172005@gmail.com>
2025-09-25 22:46:01 +0700

    thêm những phần từ branch Vinh vào main

---
+++ PizzarIO_SWP391/src/main/java/com/group1/swp/pizzario_swp391/dto/UpdateProductRequest.java
@@ -0,0 +1,25 @@
+package com.group1.swp.pizzario_swp391.dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UpdateProductRequest {
+    Long categoryId;
+    String name;
+    String description;
+    String imageURL;
+    double basePrice;
+    double flashSalePrice;
+    LocalDateTime flashSaleStart;
+    LocalDateTime flashSaleEnd;
+    boolean isActive;
+}
+
+
