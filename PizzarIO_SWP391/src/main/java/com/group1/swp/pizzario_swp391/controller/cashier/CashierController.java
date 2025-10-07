cdf95bdf1599449d9f6be27fd8af555b62a60b32
Haichan <160374468+DaeyangTQH@users.noreply.github.com>
2025-10-07 08:06:27 +0700

    Update(structure): Websocket layer

--- PizzarIO_SWP391/src/main/java/com/group1/swp/pizzario_swp391/controller/cashier/CashierController.java
+++ PizzarIO_SWP391/src/main/java/com/group1/swp/pizzario_swp391/controller/cashier/CashierController.java
@@ -1,18 +1,25 @@
 package com.group1.swp.pizzario_swp391.controller.cashier;
 
+import com.group1.swp.pizzario_swp391.service.TableService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("cashier")
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
 public class CashierController {
 
+    TableService tableService;
+
     @GetMapping
-    public String cashierHome() {
+    public String cashierHome(Model model) {
+        model.addAttribute("tables", tableService.getTablesForCashier());
         return "cashier-page/cashier";
     }
-
-
-
 }
