6f8b096f02dd68d8edd6f03b98de8aee8099a922
Haichan <160374468+DaeyangTQH@users.noreply.github.com>
2025-10-17 18:28:42 +0700

    Create(dto): Create new dto for update reservation

---
+++ PizzarIO_SWP391/src/main/java/com/group1/swp/pizzario_swp391/dto/reservation/ReservationUpdateDTO.java
@@ -0,0 +1,45 @@
+package com.group1.swp.pizzario_swp391.dto.reservation;
+
+import java.time.LocalDateTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReservationUpdateDTO {
+    
+    private Long id;
+    
+    @NotBlank(message = "Tên khách hàng không được để trống")
+    private String customerName;
+    
+    @Pattern(regexp = "^(?:\\+84|0)\\d{9}$", message = "Số điện thoại không hợp lệ (phải bắt đầu bằng 0 hoặc +84 và có 10-12 số)")
+    private String customerPhone;
+    
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    @Future(message = "Thời gian đặt bàn phải là thời điểm trong tương lai")
+    private LocalDateTime startTime;
+    
+    @Min(value = 1, message = "Số lượng khách không được ít hơn 1 người")
+    @Max(value = 10, message = "Số lượng khách không được vượt quá 10 người")
+    private int capacityExpected;
+    
+    private String note;
+    
+    private Integer tableId;
+
+}
+
+
+
+
